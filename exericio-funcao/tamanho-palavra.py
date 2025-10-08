06129aab247fc93a3afe1284de6280e688831dfc
Anderson Gomes <dev.andersong@gmail.com>
2025-10-08 10:30:17 -0300

    Adicionar função para calcular o tamanho de uma palavra

---
+++ exericio-funcao/tamanho-palavra.py
@@ -0,0 +1,11 @@
+"""
+Função que mostra o tamanho de uma palavra
+
+    Crie uma função chamada tamanho_palavra(palavra) que retorna quantas letras tem a palavra.
+"""
+
+
+def tamanho_palavra(palavra):
+    print(len(palavra))
+
+tamanho_palavra("Anderson")
