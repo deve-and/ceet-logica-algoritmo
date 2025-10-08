34636170ead3fee4269e55b05c47e37b042bce6d
Anderson Gomes <dev.andersong@gmail.com>
2025-10-08 10:38:08 -0300

    Adicionar função para transformar texto em maiúsculas

---
+++ exericio-funcao/txt-maiusculo.py
@@ -0,0 +1,8 @@
+"""
+Função que transforma texto em maiúsculas
+    Crie uma função chamada em_maiusculas(texto) que retorna o texto todo em letras maiúsculas.
+"""
+def em_maiuscula(texto):
+    print(texto.upper())
+
+em_maiuscula("texto")
