6df60763a1dfe2013c94561fbefaf70e61f5df6a
Anderson Gomes <dev.andersong@gmail.com>
2025-10-08 11:04:42 -0300

    Adicionar função para contar quantos nomes existem em uma lista

---
+++ exericio-funcao/contar_nomes.py
@@ -0,0 +1,11 @@
+"""
+Função que conta quantos nomes tem uma lista
+    Crie uma função chamada contar_nomes(lista_de_nomes) que retorna quantos nomes existem na lista.
+"""
+lista_de_nomes = "anderson", "gomes", "silva"
+
+def contar_nomes(lista_de_nomes):
+    return print(len(lista_de_nomes))
+
+
+contar_nomes(lista_de_nomes)
