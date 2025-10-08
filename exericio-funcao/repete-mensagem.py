cf0506629df4e02a2880e282b498a680bc5bdc0a
Anderson Gomes <dev.andersong@gmail.com>
2025-10-08 10:25:43 -0300

    Adicionar função para repetir uma mensagem

---
+++ exericio-funcao/repete-mensagem.py
@@ -0,0 +1,12 @@
+"""
+Função que repete uma mensagem
+    Crie uma função chamada repetir_mensagem(msg, vezes) que imprime a mensagem msg o número de vezes indicado por vezes.
+"""
+
+def repetir_mensagem(msg, vezes):
+    for i in range(vezes):
+        print(msg)
+
+repetir_mensagem("Olá", 3)
+
+
