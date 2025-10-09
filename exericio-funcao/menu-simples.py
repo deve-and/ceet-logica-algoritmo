b216628bfb1f161b10ca68fb16e71e34d2f8bfb8
Anderson Gomes <dev.andersong@gmail.com>
2025-10-09 12:36:21 -0300

    Adicionar função para exibir um menu simples com opções fictícias

---
+++ exericio-funcao/menu-simples.py
@@ -0,0 +1,15 @@
+"""
+Função que exibe um menu simples
+
+    Crie uma função chamada mostrar_menu() que imprime três opções fictícias (ex: 1 - Iniciar, 2 - Ajuda, 3 - Sair).
+"""
+
+
+def mostrar_menu():
+    print("""
+1 - Iniciar
+2 - Ajuda
+3 - Sair
+""")
+    
+mostrar_menu()
