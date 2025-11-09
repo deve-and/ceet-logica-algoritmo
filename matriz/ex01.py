6461a3f70a483fa571f2b23ece0630e3b250aede
Anderson Gomes <dev.andersong@gmail.com>
2025-11-09 15:57:47 -0300

    Adicionar exercícios para criar e exibir uma matriz 3x3 e mostrar seu primeiro elemento

---
+++ matriz/ex01.py
@@ -0,0 +1,22 @@
+"""
+Crie uma matriz 3x3 com números inteiros à sua escolha e exiba todos
+os elementos na tela.
+Exemplo de saída:
+1 2 3
+4 5 6
+7 8 9
+"""
+
+matriz = [
+        [7, 8, 9],
+        [4, 5, 6],
+        [1, 2, 3]
+        ]
+
+
+for row in matriz:
+        for elem in row:
+            print(elem, end=' ')
+        print()
+
+
---
+++ matriz/ex02.py
@@ -0,0 +1,7 @@
+"""
+Mostre o primeiro elemento da matriz criada na questão anterior.
+"""
+
+from ex01 import matriz
+
+print(matriz[0][0])
